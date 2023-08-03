5ffee3c29ed5c2ca4d9cda15dba23408264992f2
Min Khant <113674028+Rayy-007@users.noreply.github.com>
2023-08-03 22:01:45 +0630

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+/* Show Detail Image SLide */
+const mainImg = document.getElementById("main--image");
+const smallImg = document.getElementsByClassName("small-img");
+
+for (const img of smallImg) {
+  img.onclick = () => {
+    mainImg.src = img.src;
+  };
+}
+
+// Go to Detail page
+const productCard = document.getElementsByClassName("pro");
+
+for (const card of productCard) {
+  card.onclick = () => {
+    location.href = "sproduct.html";
+  };
+}
