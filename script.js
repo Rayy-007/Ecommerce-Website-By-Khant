31dc184ba7388eddcc7e843f9950ef6c400369a6
Min Khant <113674028+Rayy-007@users.noreply.github.com>
2023-08-04 21:54:19 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Mobile Navigation functionality */
 const mobileBar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
@@ -17,7 +18,6 @@ if (close) {
 /* Show Detail Image SLide */
 const mainImg = document.getElementById("main--image");
 const smallImg = document.getElementsByClassName("small-img");
-
 for (const img of smallImg) {
   img.onclick = () => {
     mainImg.src = img.src;
@@ -26,7 +26,6 @@ for (const img of smallImg) {
 
 // Go to Detail page
 const productCard = document.getElementsByClassName("pro");
-
 for (const card of productCard) {
   card.onclick = () => {
     location.href = "sproduct.html";
@@ -37,11 +36,13 @@ for (const card of productCard) {
 const readMore = document.getElementById("read-more");
 const readMoreText = document.getElementById("read-more-text");
 
+// To show the text
 const showReadMoreText = () => {
   readMore.textContent = "read less";
   readMoreText.style.display = "inline";
 };
 
+// To hide the text
 const hideReadMoreText = () => {
   readMore.textContent = "read more...";
   readMoreText.style.display = "none";
@@ -60,6 +61,7 @@ if (readMore) {
   });
 }
 
+// if the screen size is lager than 799 px the read more text will be shown
 window.addEventListener("resize", () => {
   if (window.innerWidth >= 799) {
     showReadMoreText();
